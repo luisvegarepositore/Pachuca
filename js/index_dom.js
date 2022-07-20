2beda7b94d7c4fffa56730d05db64ea019270fe1
luisvegarepositore <109642759+luisvegarepositore@users.noreply.github.com>
2022-07-20 00:21:45 -0500

    Add files via upload

---
+++ js/index_dom.js
@@ -0,0 +1,6 @@
+import hamburguerMenu from "./menu_hamburguesa.js";
+
+const d = document;
+d.addEventListener("DOMContentLoaded",e=>{
+        hamburguerMenu(".hamburger",".principal_header",".principal_header a");
+})
