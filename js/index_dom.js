362f67befe3bd8d995f61af27b680193ba8bd68b
luisvegarepositore <109642759+luisvegarepositore@users.noreply.github.com>
2022-07-21 00:24:08 -0500

    Add files via upload

--- js/index_dom.js
+++ js/index_dom.js
@@ -4,3 +4,10 @@ const d = document;
 d.addEventListener("DOMContentLoaded",e=>{
         hamburguerMenu(".hamburger",".principal_header",".principal_header a");
 })
+
+var swiper = new Swiper(".mySwiper", {
+        pagination: {
+          el: ".swiper-pagination",
+          dynamicBullets: true,
+        },
+      });
